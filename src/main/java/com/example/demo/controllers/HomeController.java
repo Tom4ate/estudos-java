c19b64041186f136c9fa83a1e819224402735065
Lucas <lucas.xerox@hotmail.com>
2026-05-09 11:15:58 -0300

    add live reload and some java home info

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,14 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    // @GetMapping("/home")
-    // public String home(@RequestParam(defaultValue = "Lucas") String name) {
-    //     return "Hello " + name;
-    // }
-
     @GetMapping("/")
-    public String index() {
-        return "Nothing to see here";
+    public HashMap<String, String> index() {
+        return new HashMap<String, String>() {{
+            put("message", "Your server is running!");
+            put("time", String.valueOf(System.currentTimeMillis()));
+        }};
     }
 
 
@@ -22,7 +22,11 @@ public class HomeController {
     public String home() {
         return "Hello ";
     }
-
     
+    // @GetMapping("/home")
+    // public String home(@RequestParam(defaultValue = "Lucas") String name) {
+    //     return "Hello " + name;
+    // }
+
 
 }
